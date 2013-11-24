4d403f1f31ef620f6014c948a34a9865ca2e1141
Lisa <lisaohlisa@gmail.com>
2013-11-23 19:16:49 -0500

    Math functions almost done, need to grab budget input and use in other functions

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-var budgetTotal // grab number entered into #total-budget
-var totalRemaining // calculated by budgetTotal - 
-
-//constructor function newPerson
-function NewPerson(name){
-    this.name = name;
-    this.actual = null;
-    this.active = true;
-    }
-
 //sample data array
 var list = [{name : 'Mom', actual : '78.00', active : true}, 
            {name : 'Kimberly', actual : '32.78', active : true}, 
@@ -15,25 +5,75 @@ var list = [{name : 'Mom', actual : '78.00', active : true},
            {name : 'Yasmine', actual : '45.00', active : true}, 
            {name : 'Amy', actual : '28.00', active : true}];
 
-//math functions
-    //remaining budget = total budget - actual spent
-    //remaining budget divided by number of active list items = per person
-    //count how many list items are active
+//constructor function newPerson
+function Person(name){
+    this.name = name;
+    this.actual = null;
+    this.active = true;
+    }
+    
+function writeList (person){
+        //check for missing person name data
+        if (typeof person.name == 'undefined'){
+            throw "Name is missing!";
+        } 
+        else {
+        $("<li class='list-group-item cf'>" + person.name + "<div class='actual-spent'>$<input type='text' class='amount' placeholder='0.00'></div>" + "</li>").appendTo('.list-group');
+        }
+    }
 
+function addPerson(name) {
+    var newPerson = new Person(name);
+    list.push(newPerson);
+    writeList(newPerson);
+    }
+
+//var budgetTotal = 1000;//test data
+var actualSpent = 45.53;//test data
+
+//count how many list items are active
 function countActive(){
     var activeCount = 0;
     for (var i=0; i < list.length; i++){
-        if (list.active == true){
-            
+        if (list[i].active == true){
+        activeCount++;
         }
     }
+    return activeCount;
 }
-
-var totalRemaining = 1000;
-
+//calculate average budget per person
 function getAverage(){
-    var average = (totalRemaining / activeCount);
+    var activeCount = countActive();
+    //remaining budget = total budget - actual spent
+    var remaining = (budgetTotal - actualSpent);
+    $('#remaining').append('$' + remaining.toFixed(2));
+    var average = (remaining / activeCount);
+    $('#per-person').append('$' + average.toFixed(2));
     }
+
+/*function totalSpent(){
+    actualSpent = justSpent + actualSpent;
+}*/
+
+$(document).ready(function() {
+    //create list
+    $("<ul class='list-group'>").appendTo(".panel-body");
+    for (var i=0; i < list.length; i++){
+        try {
+            writeList(list[i]);
+        }catch(error){
+            console.log("Error:" + error);
+        }
+    }//end error handling
+    
+    $('#total-budget').on('submit', function(event){
+        event.preventDefault();
+        var budgetTotal = $('#budget-input').val();
+        $('#initial-budget').append('$' + budgetTotal);
+        $('#budget-input').val('');
+    });
+     
+});
 
 
 
